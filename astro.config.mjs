317c13595681c289d8246d4d9ceee98f63576981
vladimir <vladimir.rdguez@gmail.com>
2026-07-30 16:17:37 -0400

    allow clodflare tunels for development

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   base: '/CartaCuba-web',
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+  },
 });
